a8f3e8ec4adf1866e7c1c1d2a3947088ba01e0a8
JUESOP <sinangun@naver.com>
2022-02-22 00:00:54 +0900

    submit

---
+++ app.js
@@ -0,0 +1,11 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit() {
+    const username = loginInput.value;
+   
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+
